e6b37179755bf14fa57f18d8d48c8d10b5a74149
Qing Ma <qma5@bcst01.parkland.edu>
2022-05-03 16:06:49 -0500

    added cake

--- exit.php
+++ exit.php
@@ -10,13 +10,13 @@ validate_or_bounce();
 
 
 session_destroy();
-header("refresh: 3; url=login.php");
+header("refresh: 5; url=login.php");
 
 
 ?>
 </head>
 
 <body>
-Good bye, Redirecting in 3 seconds.
+Good bye, Redirecting in 5 seconds.
 </body>
 </html>
