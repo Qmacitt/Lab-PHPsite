f73fbc628cda2ce8c7fba79f6fd6702f3d6a2223
Qing Ma <qma5@bcst01.parkland.edu>
2022-05-03 00:00:07 -0500

    added lib

---
+++ lib/footer.php
@@ -0,0 +1,4 @@
+<?php
+echo "User: <b><code>" . $_SESSION['username'] . "</code></b>";
+echo date("F j, Y, g:i a");      
+?>
---
+++ lib/phpfunctions.php
@@ -0,0 +1,43 @@
+<?php
+
+function validate_or_bounce()
+{
+    if (!isset($_SESSION['username']))
+    {
+        header("Location: login.php");
+    }
+}
+
+function showPost($key) 
+{
+    if (isset($_POST[$key]))
+    {
+        echo htmlspecialchars($_POST[$key]);
+    }
+}
+
+function getPost($key) 
+{
+    if (isset($_POST[$key]))
+    {
+        return htmlspecialchars($_POST[$key]);
+    }
+    else
+    {
+        return "";
+    }
+}
+
+function validate_login($username, $password)
+{
+    if ($username=="root" && $password=="pass123")
+    {
+	      return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+?>
